a4332057902a39fc7b93e8b78901765f30322358
Unknown <danielivansauceda@gmail.com>
2017-03-03 01:02:08 -0600

    Clase trabajador

--- src/ejercicios/Trabajador.java
+++ src/ejercicios/Trabajador.java
@@ -1,5 +1,48 @@
 package ejercicios;
 
 public class Trabajador {
+        
+	String nombre;
+	char sexo;
+	int horas;
+	
+	
+	public Trabajador(String nombre, char sexo, int horas) {
+		super();
+		this.nombre = nombre;
+		this.sexo = sexo;
+		this.horas = horas;
+	}
 
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public char getSexo() {
+		return sexo;
+	}
+
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+
+	public int getHoras() {
+		return horas;
+	}
+
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	
+	
 }
